7d38f1feabeb604194626e7eff04d1e57bd1d008
snadell <sravya.nadella7@gmail.com>
2017-07-03 00:32:40 -0400

    added new

--- data/api.py
+++ data/api.py
@@ -13,6 +13,25 @@ def accounts():
     token = request.headers.get('token');
     if not dao.authenticate(token):
         return Response(status=401)
+        if request.method == "GET":
+        return Response(json_util.dumps(dao.listAccounts()), mimetype='application/json')
+    elif request.method == "POST":
+        name = request.args.get("name");
+        employees = int(request.args.get("employees")) or 0
+        valuation = int(request.args.get("valuation")) or 0
+        if not name or len(name) == 0:
+            r = json.jsonify(reason="Name is required.");
+            r.status_code=400;
+            return r;
+        else:
+            try:
+                id = dao.insertAccount(name, employees, valuation);
+                return Response(json_util.dumps({"id": id}), mimetype='application/json', status=201);
+            except:
+                r = json.jsonify(reason="Failed to create account.");
+                r.status_code=500;
+                return r;
+
 @app.route("/health")
 def health():
     logger.debug("Get Health");
